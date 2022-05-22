a9c9dd986f9187c8db082d9a8a6c7ea18ac6525e
aniaklr <92811464+aniaklr@users.noreply.github.com>
2022-05-22 16:48:19 +0200

    moving css files

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,31 @@
+define(function() {var keywords=[{w:"Apteczka",p:["p0"]},{w:"Zdarzenie",p:["p1"]},{w:"Context",p:["p2"]},{w:"Sensitive",p:["p2"]},{w:"Help",p:["p2"]},{w:"Atak",p:["p3"]},{w:"paniki",p:["p3"]},{w:"Krwawienie",p:["p4"]},{w:"Obra\u017Cenia",p:["p5"]},{w:"oczu",p:["p5"]},{w:"Odmro\u017Cenie/hipotermia",p:["p6"]},{w:"Oparzenie",p:["p7","p8"]},{w:"ciep\u0142em",p:["p7"]},{w:"substancj\u0105",p:["p8"]},{w:"Otarcia",p:["p9"]},{w:"Pora\u017Cenie",p:["p10"]},{w:"pr\u0105dem",p:["p10"]},{w:"Rany",p:["p11","p12","p13"]},{w:"ci\u0119te",p:["p11"]},{w:"k\u0142ute",p:["p12"]},{w:"szarpane",p:["p13"]},{w:"Udar",p:["p14"]},{w:"cieplny",p:["p14"]},{w:"Wst\u0119p",p:["p15"]},{w:"Zad\u0142awienie",p:["p16"]},{w:"Zatrucie",p:["p17","p18"]},{w:"u",p:["p17","p18"]},{w:"psa",p:["p17"]},{w:"kota/gryzonia",p:["p18"]},{w:"Z\u0142amanie",p:["p19"]},{w:"Akcesoria",p:["p20"]},{w:"Artyku\u0142y",p:["p21"]},{w:"spo\u017Cywcze",p:["p21"]},{w:"Preparaty",p:["p22"]},{w:"apteczne",p:["p22"]},{w:"Wyposa\u017Cenie",p:["p23"]},{w:"apteczki",p:["p23"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2, 3, 4];
+ph["p3"]=[5, 6];
+ph["p4"]=[7];
+ph["p5"]=[8, 9];
+ph["p6"]=[10];
+ph["p7"]=[11, 12];
+ph["p8"]=[11, 13];
+ph["p9"]=[14];
+ph["p10"]=[15, 16];
+ph["p21"]=[31, 32];
+ph["p20"]=[30];
+ph["p12"]=[17, 19];
+ph["p23"]=[35, 36];
+ph["p11"]=[17, 18];
+ph["p22"]=[33, 34];
+ph["p14"]=[21, 22];
+ph["p13"]=[17, 20];
+ph["p16"]=[24];
+ph["p15"]=[23];
+ph["p18"]=[25, 26, 28];
+ph["p17"]=[25, 26, 27];
+ph["p19"]=[29];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
