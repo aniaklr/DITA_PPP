fd9a9ed26fa458196d4f67a64913ad4559fea066
aniaklr <92811464+aniaklr@users.noreply.github.com>
2022-05-22 16:10:07 +0200

    all final project files

---
+++ pierwsza pomoc przedweterynaryjna/topics/out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {5:1,19:1,8:1,17:-1,40:0,16:1,13:1,39:0,7:1,14:1,15:1,0:-1,10:1,9:1,38:0,6:1,21:1,18:1,20:1,11:1,1:-1,12:1};
+});
